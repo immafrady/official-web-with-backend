91cc7e6075c40cdb43fe268e2b48d419ede11a73
Frady Wang <immafrady@gmail.com>
2020-06-12 18:34:08 +0800

    chore: Delete Angular Material

--- client-side/src/app/app.browser.module.ts
+++ client-side/src/app/app.browser.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {AppModule} from "./app.module";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     AppModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    BrowserTransferStateModule,
-    BrowserAnimationsModule
+    BrowserTransferStateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
