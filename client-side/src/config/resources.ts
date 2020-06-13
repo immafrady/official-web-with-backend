c2ba15fce31462f98844eca2b460322ed425b1c4
Frady Wang <immafrady@gmail.com>
2020-06-13 18:43:44 +0800

    chore: resource update

---
+++ client-side/src/config/resources.ts
@@ -0,0 +1,17 @@
+// 公司信息
+export const COMPANY_INFO = {
+  ADDRESS: '广州市天河区力达广场A1栋16层',
+  TELEPHONES: ['020-88529619', '020-38776616'],
+  WEIBO_HREF: '',
+  WECHAT_QR_CODE: '',
+  COPYRIGHT: '2019',
+  BEIAN: '粤ICP备17126097号'
+}
+
+// 工信部地址，怕他又变
+export const GONG_XIN_BU_URL = 'http://www.beian.gov.cn/portal/index.do'
+
+// base64图片
+export const BASE_64_IMG = {
+
+}
