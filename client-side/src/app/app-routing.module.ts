e67819d1da93ad6717d05a2a9aa23566df3fa7d7
Frady Wang <immafrady@gmail.com>
2020-06-14 01:36:37 +0800

    feat: 使用路由懒加载

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from "./app.component";
 import {ErrorComponent} from "./error/error.component";
-import { PcComponent } from "./modules/pc/pc.component";
 
 
 const routes: Routes = [
   { path: '', component: AppComponent, children: [
-      { path: '', component: PcComponent },
+      { path: '', loadChildren: () => import('./modules/pc/pc.module').then(mod => mod.PcModule) },
       { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(mod => mod.AdminModule) }
     ]
   },
--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
-import {PcModule} from "./modules/pc/pc.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    PcModule,
     AppRoutingModule,
     HttpClientModule
   ],
