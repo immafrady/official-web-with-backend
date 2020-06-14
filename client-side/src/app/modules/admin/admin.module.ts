eb09f946d6a8ae9080470877932d405dc557fb6c
Frady Wang <immafrady@gmail.com>
2020-06-14 22:59:38 +0800

    feat: Adding admin module; docs: update README.md

--- client-side/src/app/modules/admin/admin.module.ts
+++ client-side/src/app/modules/admin/admin.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule
   ]
 })
 export class AdminModule { }
