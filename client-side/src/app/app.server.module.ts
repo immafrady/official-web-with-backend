c2c7c02a644c43dc5ea3f87b019171477b769d53
Frady Wang <immafrady@gmail.com>
2020-06-15 01:56:36 +0800

    chore: 增加必要的配置

--- client-side/src/app/app.server.module.ts
+++ client-side/src/app/app.server.module.ts
@@ -3,9 +3,11 @@ import {ServerModule, ServerTransferStateModule} from '@angular/platform-server'
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { UniversalInterceptor } from "./universal.interceptor";
 import { BrowserModule } from "@angular/platform-browser";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {NZ_I18N, zh_CN, NzI18nModule} from "ng-zorro-antd/i18n";
 
 @NgModule({
   imports: [
@@ -13,12 +15,18 @@ import { BrowserModule } from "@angular/platform-browser";
     ServerModule,
     ServerTransferStateModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    NoopAnimationsModule,
+    HttpClientModule,
+    NzI18nModule
   ],
   bootstrap: [AppComponent],
   providers: [{
     provide: HTTP_INTERCEPTORS,
     useClass: UniversalInterceptor,
     multi: true
+  }, {
+    provide: NZ_I18N,
+    useValue: zh_CN
   }]
 })
 export class AppServerModule {}
