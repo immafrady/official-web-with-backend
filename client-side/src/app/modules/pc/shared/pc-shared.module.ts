8247563d7fed3673bb25bcdda8102faedd70e76e
Frady Wang <immafrady@gmail.com>
2020-06-15 10:20:32 +0800

    feat: 图片地址改成引用

--- client-side/src/app/modules/pc/shared/pc-shared.module.ts
+++ client-side/src/app/modules/pc/shared/pc-shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import {GlobalSharedModule} from "../../../shared/global-shared.module";
 import {CoreContainerComponent} from "./core-container/core-container.component";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [CoreContainerComponent],
   imports: [
+    CommonModule,
     GlobalSharedModule
   ],
   exports: [
--- client-side/src/config/resources.ts
+++ client-side/src/config/resources.ts
@@ -4,14 +4,9 @@ export const COMPANY_INFO = {
   ADDRESS: '广州市天河区力达广场A1栋16层',
   TELEPHONES: ['020-88529619', '020-38776616'],
   WEIBO_HREF: '',
-  COPYRIGHT: '2019',
+  COPYRIGHT: 'Copyright 2019',
   BEIAN: '粤ICP备17126097号'
 }
 
 // 工信部地址，怕他又变
 export const GONG_XIN_BU_URL = 'http://www.beian.gov.cn/portal/index.do'
-
-// base64图片
-export const BASE_64_IMG = {
-
-}
