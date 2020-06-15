92e36851216d1f3e3e6f787253d80c87aa0b3c83
Frady Wang <immafrady@gmail.com>
2020-06-15 17:13:46 +0800

    feat: 首页底部轮播完成

--- client-side/src/app/modules/pc/pages/home/home.module.ts
+++ client-side/src/app/modules/pc/pages/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import {NzButtonModule, NzCarouselModule} from "ng-zorro-antd";
+import {NzButtonModule, NzCarouselModule, NzGridModule} from "ng-zorro-antd";
 
 import { HomeRoutingModule } from './home-routing.module';
 import {HomeComponent} from "./home.component";
 import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
 import {PcSharedModule} from "../../shared/pc-shared.module";
 import {CommonModule} from "@angular/common";
+import {NzSpaceModule} from "ng-zorro-antd/space";
 
 @NgModule({
   declarations: [HomeComponent, HomeCarouselComponent],
@@ -14,7 +15,9 @@ import {CommonModule} from "@angular/common";
     PcSharedModule,
     HomeRoutingModule,
     NzCarouselModule,
-    NzButtonModule
+    NzButtonModule,
+    NzGridModule,
+    NzSpaceModule
   ],
 })
 export class HomeModule { }
