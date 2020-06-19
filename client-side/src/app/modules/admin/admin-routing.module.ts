9e0002feecb0f513091a6803f7fe5c49825d8d9f
Frady Wang <immafrady@gmail.com>
2020-06-19 14:05:51 +0800

    feat[client]: 侧边栏

--- client-side/src/app/modules/admin/admin-routing.module.ts
+++ client-side/src/app/modules/admin/admin-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AdminComponent} from "./admin.component";
+import { WrapperComponent } from "./layout/wrapper/wrapper.component";
+import { LoginComponent } from "./pages/login/login.component";
 
 
 const routes: Routes = [
-  { path: '', component: AdminComponent }
+  { path: '', component: AdminComponent, children: [
+      { path: 'login', component: LoginComponent },
+      { path: '', component: WrapperComponent, children: [
+        // todo 加载具体页面
+        ] },
+    ]
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
--- client-side/src/app/modules/admin/admin.module.ts
+++ client-side/src/app/modules/admin/admin.module.ts
@@ -4,14 +4,21 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import {AdminSharedModule} from "./shared/admin-shared.module";
 import {CommonModule} from "@angular/common";
+import { NzBreadCrumbModule, NzIconModule, NzLayoutModule, NzMenuModule } from "ng-zorro-antd";
+import { WrapperComponent } from './layout/wrapper/wrapper.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, WrapperComponent, LoginComponent],
   imports: [
     CommonModule,
     AdminSharedModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzIconModule
   ]
 })
 export class AdminModule { }
