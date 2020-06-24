5f0f0b45696406a29b27302651f93ad5cfa249b6
huangyaqing <m15089577102@163.com>
2020-06-24 11:36:58 +0800

    feat 样式优化

--- client-side/src/app/modules/pc/pages/xin-town/xin-town.component.ts
+++ client-side/src/app/modules/pc/pages/xin-town/xin-town.component.ts
@@ -14,6 +14,7 @@ import Swiper from "swiper";
 export class XinTownComponent extends BasePageComponent implements OnInit, AfterViewInit {
   getImage = getImage;
   xinTownwiper: Swiper;
+  sliderName: string = 'prev';
   slides = [
     getImage('xin-town-big-bg-1'),
     getImage('xin-town-small-bg-2'),
@@ -26,15 +27,18 @@ export class XinTownComponent extends BasePageComponent implements OnInit, After
   ) {
     super(metaService, titleService, activatedRoute, router)
   }
-  swipePrev() {
+  swipePrev(name) {
     this.xinTownwiper.swipePrev();
+    this.sliderName = name
   }
-  swipeNext() {
+  swipeNext(name) {
     this.xinTownwiper.swipeNext();
+    this.sliderName = name
   }
   ngAfterViewInit() {
     this.xinTownwiper = new Swiper('#gallery', {
       spaceBetween: 10,
+      autoplay: 1000,
       calculateHeight: true,
       thumbs: {
         swiper: {
