212c29f9c4b90dff9b814bf0dc45cd4f3d837cf8
huangyaqing <m15089577102@163.com>
2020-07-02 11:36:37 +0800

    temp 微博增加跳转链接

--- client-side/src/config/resources.ts
+++ client-side/src/config/resources.ts
@@ -3,7 +3,7 @@ export const APPLICATION_NAME = '薪宝科技'
 export const COMPANY_INFO = {
   ADDRESS: '广州市天河区力达广场A1栋16层（整层）',
   TELEPHONES: ['020-88529619', '020-38776616'],
-  WEIBO_HREF: '',
+  WEIBO_HREF: 'http://weibo.com/xinshuiguanjia',
   COPYRIGHT: 'Copyright 2019',
   BEIAN: '粤ICP备17126097号-4'
 }
