464321f92d6567017563fe7f78a4f90c05b9b403
王兆炜 <wangzw@xinshuibao1.com>
2020-08-05 11:52:36 +0800

    feat[server]: 增加迁移的sql脚本

---
+++ server-side/migration/003.add-picture-table.sql
@@ -0,0 +1,14 @@
+create table picture
+(
+    id          int         auto_increment      primary key ,
+    createDate  datetime(6)                     default CURRENT_TIMESTAMP(6)        not null ,
+    updateDate  datetime(6)                     default CURRENT_TIMESTAMP(6)        not null ,
+    modifyDate  datetime(6)                     default CURRENT_TIMESTAMP(6)        not null ,
+    url         text                                                                not null ,
+    comment     text                                                                not null ,
+    info        text                                                                not null ,
+    priority    enum('0', '1')                  default '0'                         not null ,
+    type        enum('ENVIRONMENT', 'FRIEND')   default 'FRIEND'                    not null ,
+    userId      int                                                                 null ,
+    constraint  FK_picture_to_user_id  foreign key (userId)    references official_web.user (id)
+);
