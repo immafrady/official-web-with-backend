12dd136bb59a802aad1d7f3b60ee1a846316d05a
huangyaqing <m15089577102@163.com>
2020-08-07 10:57:13 +0800

    feat 【图片管理】增加缩略图放大功能

--- client-side/src/app/modules/admin/pages/picture-manager/picture-manager/picture-manager.component.ts
+++ client-side/src/app/modules/admin/pages/picture-manager/picture-manager/picture-manager.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { httpParamsHandler } from "@/utils/httpParamsHandler";
 import { PictureManagerService } from "@admin/pages/picture-manager/picture-manager.service";
 import { IPictureEntity } from "@libs/entity/picture";
+import {GALLERY_CONF, GALLERY_IMAGE, NgxImageGalleryComponent} from "@web-aid-kit/ngx-image-gallery";
 
 @Component({
   selector: 'admin-picture-manager',
@@ -10,12 +11,25 @@ import { IPictureEntity } from "@libs/entity/picture";
   styleUrls: ['./picture-manager.component.scss']
 })
 export class PictureManagerComponent implements OnInit {
+  @ViewChild(NgxImageGalleryComponent) ngxImageGallery: NgxImageGalleryComponent;
   pictureList: IPictureEntity[];
   total: number;
   checked = false;
   setOfCheckedId = new Set<number>();
+  conf: GALLERY_CONF = {
+    imageOffset: '0px',
+    showDeleteControl: false,
+    showImageTitle: true,
+  };
+  imgList: GALLERY_IMAGE[] = [];
   constructor(private pictureManagerService: PictureManagerService) { }
 
+  openGallery(url, title, index: number = 0) {
+    this.imgList = [{ url, title }];
+    setTimeout(() => {
+      this.ngxImageGallery.open();
+    }, 0)
+  }
   ngOnInit(): void {
     this.getPictureList();
   }
@@ -25,7 +39,7 @@ export class PictureManagerComponent implements OnInit {
    */
   getPictureList() {
     this.pictureManagerService.getPictureList().subscribe(res => {
-      this.total = res.data.total
+      this.total = res.data.total;
       this.pictureList = res.data.list
     })
   }
