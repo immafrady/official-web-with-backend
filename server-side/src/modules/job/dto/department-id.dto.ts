9a5186f3aa45d89293ab5cf623506b37ad47e35b
王兆炜 <wangzw@xinshuibao1.com>
2020-08-13 20:01:01 +0800

    feat[server]: job detail - 补充详情返回的department id

--- server-side/src/modules/job/dto/department-id.dto.ts
+++ server-side/src/modules/job/dto/department-id.dto.ts
@@ -2,7 +2,7 @@ import { IsNumberString } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class DepartmentIdDto {
-    @ApiProperty({ description: '职位Id' })
+    @ApiProperty({ description: '部门Id' })
     @IsNumberString()
     id: number;
 }
