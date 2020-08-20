42265a9b2048615970074b4b401985dd5d542f2d
王兆炜 <wangzw@xinshuibao1.com>
2020-08-20 11:38:39 +0800

    perf[client]: 侧边栏改为配置型

--- client-side/src/app/modules/admin/layout/wrapper/wrapper.component.ts
+++ client-side/src/app/modules/admin/layout/wrapper/wrapper.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {getImage} from '@/utils/getImage';
 
+interface IStructureItem {
+  label: string;
+  icon?: string;
+  link?: string;
+  children?: IStructureItem[]
+}
+
 @Component({
   selector: 'app-wrapper',
   templateUrl: './wrapper.component.html',
@@ -8,6 +15,26 @@ import {getImage} from '@/utils/getImage';
 })
 export class WrapperComponent implements OnInit {
   getImage = getImage;
+
+  structure: IStructureItem[] = [{
+    label: '文章管理',
+    icon: 'form',
+    link: '/admin/news-list'
+  }, {
+    label: '图片管理',
+    icon: 'picture',
+    link: '/admin/picture-manager'
+  }, {
+    label: '招聘设置',
+    icon: 'setting',
+    children: [{
+      label: '部门管理',
+      link: '/admin/department-manager'
+    }, {
+      label: '招聘管理',
+      link: '/admin/recruitment-manager'
+    }]
+  }]
   constructor() { }
 
   ngOnInit(): void {
