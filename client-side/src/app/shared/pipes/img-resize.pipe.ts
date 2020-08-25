0a46a9bf4a114f298449de96ec18d4e9fe55c451
huangyaqing <m15089577102@163.com>
2020-08-25 18:44:09 +0800

    feat 【图片压缩过滤】修改默认值

--- client-side/src/app/shared/pipes/img-resize.pipe.ts
+++ client-side/src/app/shared/pipes/img-resize.pipe.ts
@@ -5,8 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ImgResizePipe implements PipeTransform {
 
-  transform(value: unknown, width: number,height: number, ...args: unknown[]): unknown {
-    console.log(width);
+  transform(value: string, width: number,height: number): string {
     return `${value}?x-oss-process=image/resize,m_fill,w_${width},h_${height}`;
   }
 
