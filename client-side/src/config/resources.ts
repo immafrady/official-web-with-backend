ffee4fa32c0e29458860ffcb4a55555f3f6ce743
huangyaqing <m15089577102@163.com>
2020-09-04 14:39:06 +0800

    feat：SEO默认标题改成用下划线 #task-view-3431

--- client-side/src/config/resources.ts
+++ client-side/src/config/resources.ts
@@ -16,5 +16,5 @@ export const REQUEST_AUTH_TOKEN = 'admin-token'
 
 // SEO相关
 export const SEO_DESCRIPTION = '薪宝科技是新就业形态下连接灵活用工平台与自由职业者的关键商税服务纽带。薪宝科技以产品技术研发和服务模式创新为核心驱动力，依托于SaaS和最佳报税模型，为新经济平台企业提供一站式发佣、结算、对账、开票、完税服务，为广大自由职业者提供工商税务登记、开票管理、纳税申报、商保社保、公积金缴纳等全方位商事服务。'
-export const SEO_DEFAULT_TITLE = APPLICATION_NAME + ' —— 2亿自由职业者的发佣报税服务商'
+export const SEO_DEFAULT_TITLE = APPLICATION_NAME + '_2亿自由职业者的发佣报税服务商'
 export const SEO_DEFAULT_KEYWORDS = '发佣报税,薪税优化,税务筹划,成本优化,佣金发放,费用发放'
