d69a7ea272ecb36b852181a7b00b6c1495afe384
huangyaqing <m15089577102@163.com>
2020-09-24 18:09:27 +0800

    feat：【大数据看板】大数据看板样式暂存

--- client-side/src/app/modules/pc/pages/big-data-board/big-data-board.component.ts
+++ client-side/src/app/modules/pc/pages/big-data-board/big-data-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {getImage} from "@/utils/getImage";
 
 @Component({
@@ -12,33 +12,33 @@ export class BigDataBoardComponent implements OnInit {
   mapList = [getImage('data-map1'), getImage('data-map2'), getImage('data-map3')];
 
   list = [
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
-    { status: '正在发放', merchantNo: '1045262893132255232', amount: '200.00'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '1'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '2'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '3'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '4'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '5'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '6'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '7'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '8'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '9'},
+    { status: '正在发放', merchantNo: '1045262893132255232', amount: '10'},
   ];
-  constructor() { }
 
   serviceOption = {
     title: {
       text: '月新增服务人次',
-      left: '5%',
-      top: '7%',
+      left: 30,
+      top: 20,
       textStyle: {
         color: '#fff',
-        fontSize: 16
+        fontSize: 16,
+        fontWeight: 500
       }
     },
     grid: [{
       left: '14%',
-      bottom: '32%',
-      top: '26%',
+      bottom: '21%',
+      top: '30%',
       right: '15%'
     }],
     xAxis: {
@@ -95,43 +95,44 @@ export class BigDataBoardComponent implements OnInit {
     title: {
       text: '服务自由职业者行业分布',
       left: 30,
-      top: 40,
+      top: 20,
       textStyle: {
         color: '#fff',
-        fontSize: 16
+        fontSize: 16,
+        fontWeight: 500
       }
     },
     tooltip: {
       show: false
     },
-    color:['#FFD669','#F08AFF','#5656D8','#FA863E','#5AC1DD', '#FF2D2D', '#FF14A4', '#ABFF86'],
+    color:['#05B1EF','#0FD0B6','#F8D958','#F0833F','#05CD61', '#E94C37', '#A486FA', '#BAD98E'],
     legend: {
       orient: 'vertical',
       x: '60%',      //可设定图例在左、右、居中
-      y:'center',     //可设定图例在上、下、居中
+      y: '25%',     //可设定图例在上、下、居中
       itemWidth: 10,
       itemHeight: 10,
       icon: 'circle',
       padding:[0,50,0,0],
       textStyle: {
         color: '#fff',
-        fontSize: 16
+        fontSize: 12
       },
-      data: ['直播', '社交电商', '即时配送', '共享出行', '医疗', '房地产', '在线教育 10%', '其他']
+      data: ['直播 15%', '社交电商 30%', '即时配送 15%', '共享出行 10%', '医疗 5%', '房地产 10%', '在线教育 10%', '其他 5%']
     },
     series: [
       {
         name: '服务自由职业者行业分布',
         type: 'pie',
-        radius: ['20%', '50%'],
-        center: ['30%', '50%'],
+        radius: ['30%', '45%'],
+        center: ['30%', '45%'],
         label: {
           show: false,
           position: 'center'
         },
         emphasis: {
           label: {
-            show: true,
+            show: false,
             fontSize: '12',
             fontWeight: 'bold'
           },
@@ -142,14 +143,14 @@ export class BigDataBoardComponent implements OnInit {
           }
         },
         data: [
-          {value: 15, name: '直播'},
-          {value: 30, name: '社交电商'},
-          {value: 15, name: '即时配送'},
-          {value: 10, name: '共享出行'},
-          {value: 5, name: '医疗'},
-          {value: 10, name: '房地产'},
+          {value: 15, name: '直播 15%'},
+          {value: 30, name: '社交电商 30%'},
+          {value: 15, name: '即时配送 15%'},
+          {value: 10, name: '共享出行 10%'},
+          {value: 5, name: '医疗 5%'},
+          {value: 10, name: '房地产 10%'},
           {value: 10, name: '在线教育 10%'},
-          {value: 5, name: '其他'}
+          {value: 5, name: '其他 5%'}
         ]
       }
     ]
@@ -159,10 +160,11 @@ export class BigDataBoardComponent implements OnInit {
     title: {
       text: '自由职业者收入水平分布',
       left: 30,
-      top: 40,
+      top: 30,
       textStyle: {
         color: '#fff',
-        fontSize: 16
+        fontSize: 16,
+        fontWeight: 500
       }
     },
     tooltip: {
@@ -172,23 +174,23 @@ export class BigDataBoardComponent implements OnInit {
     legend: {
       orient: 'vertical',
       x: '60%',      //可设定图例在左、右、居中
-      y:'center',     //可设定图例在上、下、居中
+      y: '22%',     //可设定图例在上、下、居中
       padding:[0,50,0,0],
       itemWidth: 10,
       itemHeight: 10,
       icon: 'circle',
       textStyle: {
         color: '#fff',
-        fontSize: 16
+        fontSize: 12
       },
-      data: ['3000元以下', '3000-4999元', '5000-7999元', '8000-11999元', '12000-14999元', '15000-19999元', '20000元以上']
+      data: ['3000元以下 20%', '3000-4999元 14%', '5000-7999元 25%', '8000-11999元 11%', '12000-14999元 13%', '15000-19999元 13%', '20000元以上 4%']
     },
     series: [
       {
         name: '自由职业者收入水平分布',
         type: 'pie',
-        radius: ['20%', '50%'],
-        center: ['30%', '50%'],
+        radius: ['20%', '45%'],
+        center: ['30%', '40%'],
         avoidLabelOverlap: false,
         label: {
           show: false,
@@ -196,7 +198,7 @@ export class BigDataBoardComponent implements OnInit {
         },
         emphasis: {
           label: {
-            show: true,
+            show: false,
             fontSize: '12',
             fontWeight: 'bold'
           },
@@ -210,13 +212,13 @@ export class BigDataBoardComponent implements OnInit {
           show: false
         },
         data: [
-          {value: 20, name: '3000元以下'},
-          {value: 14, name: '3000-4999元'},
-          {value: 25, name: '5000-7999元'},
-          {value: 11, name: '8000-11999元'},
-          {value: 13, name: '12000-14999元'},
-          {value: 13, name: '15000-19999元'},
-          {value: 4, name: '20000元以上'}
+          {value: 20, name: '3000元以下 20%'},
+          {value: 14, name: '3000-4999元 14%'},
+          {value: 25, name: '5000-7999元 25%'},
+          {value: 11, name: '8000-11999元 11%'},
+          {value: 13, name: '12000-14999元 13%'},
+          {value: 13, name: '15000-19999元 13%'},
+          {value: 4, name: '20000元以上 4%'}
         ]
       }
     ]
@@ -226,6 +228,7 @@ export class BigDataBoardComponent implements OnInit {
     this.activeMap = val
   };
 
+  constructor() {}
 
   ngOnInit(): void {
   }
